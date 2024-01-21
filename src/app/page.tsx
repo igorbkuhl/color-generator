209e291424f9a5ed30f3ce9d27a7e95089dc7307
igorbkuhl <igorb.kuhl@gmail.com>
2024-01-21 13:17:10 -0300

    Styles moved to Generator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,11 @@
 "use client";
 
-import { useEffect } from "react";
 import Generator from "@/components/Generator";
-import useRandColor from "@/hooks/useRandColor";
 
 export default function Home() {
-  const { color, setColor, newColor } = useRandColor();
+
   return (
-    <main
-      className="flex justify-center items-center h-screen"
-      style={{
-        backgroundColor: `${color}`,
-      }}
-    >
+    <main>
       <Generator />
     </main>
   );
