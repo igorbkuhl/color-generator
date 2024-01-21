42d8e65c9a4dad59972598861441123fcc5dd5aa
igorbkuhl <igorb.kuhl@gmail.com>
2024-01-21 13:19:17 -0300

    Color generator working properly

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -1,17 +1,52 @@
-import { useState, useEffect } from "react";
-import dynamic from "next/dynamic";
-import useRandColor from "@/hooks/useRandColor";
-import Button from "@/components/Button"
+import { Component } from "react";
 
-const NoSSR = dynamic(() => import("../components/ColorCode"), {ssr: false})
+const initialState = {
+  isCode: true,
+  colors: ["", ""],
+  currentColor: "rgb(255, 100, 72)"
+}
 
-export default function Generator() {
-  const { color, setColor, newColor, changeColor, isCode, setIsCode } = useRandColor();
+export default class Generator extends Component {
 
-  return (
-    <div className="flex flex-col">
-      <Button />
-      <NoSSR />
-    </div>
-  );
+  state = { ...initialState }
+
+  constructor(props: any) {
+    super(props)
+    this.generateColor = this.generateColor.bind(this)
+  }
+
+  generateColor() {
+    let rgb = [
+      Math.floor(Math.random() * 255),
+      Math.floor(Math.random() * 255),
+      Math.floor(Math.random() * 255)
+    ];
+    console.log(this.state.currentColor)
+    this.setState({ currentColor: `rgb(${rgb})` })
+  }
+  
+  render() {
+    return (
+      <div className="flex justify-center items-center h-screen w-screen"
+        style={{
+          backgroundColor: `${this.state.currentColor}`,
+        }}
+      >
+        <div className="flex flex-col">
+          <button
+            onClick={this.generateColor}
+            className="
+            text-white p-4 border-2 border-white rounded-md
+            bg-slate-800/50 backdrop-blur-sm"
+          >
+            Generate
+          </button>
+          <p className="bg-slate-800/50 backdrop-blur-sm
+            mt-2 p-5 rounded-md">
+            {this.state.isCode ? this.state.currentColor : "Invalid color."}
+          </p>
+        </div>
+      </div>
+    );
+  }
 }
