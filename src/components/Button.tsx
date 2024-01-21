c1ee29e82ee828a138b68d056066637738f3d408
igorbkuhl <igorb.kuhl@gmail.com>
2024-01-21 13:20:16 -0300

    Temporarily reduced

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,16 +2,5 @@ import { useEffect } from "react";
 import useRandColor from "@/hooks/useRandColor";
 
 export default function Button() {
-  const { changeColor, setColor, newColor, generateColor } = useRandColor();
 
-  return (
-    <button
-      onClick={changeColor}
-      className="
-      text-white p-4 border-2 border-white rounded-md
-      bg-slate-800/50 backdrop-blur-sm"
-    >
-      Generate
-    </button>
-  )
 }
--- src/hooks/useRandColor.ts
+++ src/hooks/useRandColor.ts
@@ -2,35 +2,26 @@ import { useState, useEffect } from "react";
 
 export default function useRandColor() {
 
-  let newColor = generateColor();
-  const [color, setColor] = useState(newColor);
-  const [isCode, setIsCode] = useState(true);
 
-  useEffect(() => {
-    changeColor();
-  }, [])
-  
   function generateColor() {
     let rgb = [
       Math.floor(Math.random() * 255),
       Math.floor(Math.random() * 255),
       Math.floor(Math.random() * 255),
     ];
-    return `rgb(${rgb})`
-  }
 
-  function changeColor() {
-    setColor(newColor)
-    console.log(color)
+    let newColor = `rgb(${rgb})`;
+    return newColor;
   }
+  
+  let newColor = generateColor();
+  let colorList = {
+    nColor: newColor
+  };
 
   return {
-    setColor,
-    color,
-    newColor,
     generateColor,
-    changeColor,
-    isCode,
-    setIsCode
+    newColor,
+    colorList
   };
 }
