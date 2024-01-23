8d477412a9d6c493bc9334490a9ec3fb2da98cdb
igorbkuhl <igorb.kuhl@gmail.com>
2024-01-23 10:24:28 -0300

    Button code moved to component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,16 @@
-import { useEffect } from "react";
-import useRandColor from "@/hooks/useRandColor";
+interface ButtonProps {
+  func: () => void
+}
 
-export default function Button() {
-
+export default function Button(props: ButtonProps) {
+  return (
+    <button
+      onClick={props.func}
+      className="
+      text-white p-4 border-2 border-white rounded-md
+      bg-slate-800/50 backdrop-blur-sm"
+    >
+      Generate
+    </button>
+  )
 }
