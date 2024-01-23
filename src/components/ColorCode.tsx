0e5515749b422e4d9f16af992f08ea1d1bb1c158
igorbkuhl <igorb.kuhl@gmail.com>
2024-01-23 10:28:16 -0300

    Color code moved to component

--- src/components/ColorCode.tsx
+++ src/components/ColorCode.tsx
@@ -1,5 +1,13 @@
-import useRandColor from "@/hooks/useRandColor";
+interface ColorCodeProps {
+  isCode: boolean,
+  currentColor: string
+}
 
-export default function ColorCode() {
- 
+export default function ColorCode(props: ColorCodeProps) {
+  return (
+    <p className="bg-slate-800/50 backdrop-blur-sm
+      mt-2 p-5 rounded-md text-center">
+      {props.isCode ? props.currentColor : "Invalid color."}
+    </p>
+  )
 }
