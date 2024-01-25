f9fe2532b8eac4180bd5abc1a73691e8492aa6a6
igorbkuhl <igorb.kuhl@gmail.com>
2024-01-25 07:13:48 -0300

    Paragraph changed to span

--- src/components/ColorCode.tsx
+++ src/components/ColorCode.tsx
@@ -1,13 +1,14 @@
 interface ColorCodeProps {
-  isCode: boolean,
-  currentColor: string
+  children: any;
 }
 
 export default function ColorCode(props: ColorCodeProps) {
   return (
-    <p className="bg-slate-800/50 backdrop-blur-sm
-      mt-2 p-5 rounded-md text-center">
-      {props.isCode ? props.currentColor : "Invalid color."}
-    </p>
+    <span className="
+    bg-slate-800/50 backdrop-blur-sm
+      mt-2 p-5 rounded-md text-center
+    ">
+      {props.children}
+    </span>
   )
 }
