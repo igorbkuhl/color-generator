921a328862527b037ac7333041a0a9119dc143c0
igorbkuhl <igorb.kuhl@gmail.com>
2024-02-06 23:49:45 -0300

    RGB text passed as props

--- src/components/ColorCode.tsx
+++ src/components/ColorCode.tsx
@@ -1,13 +1,13 @@
-import { useEffect } from "react"
-import useRandColor from "@/hooks/useRandColor";
+interface ColorCodeProps {
+  text: string;
+}
 
-export default function ColorCode() {
-  const { color, setColor, isCode, setIsCode, newColor, changeColor, generateColor } = useRandColor();
+export default function ColorCode(props: ColorCodeProps) {
  
   return (
     <p className="bg-slate-800/50 backdrop-blur-sm
-      mt-2 p-5 rounded-md">
-      {isCode ? color : newColor}
+      mt-2 p-5 rounded-md text-center">
+      {props.text}
     </p>
   )
 }
