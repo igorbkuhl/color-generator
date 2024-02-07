852522446f1f3687074b04656240aae645b20c07
igorbkuhl <igorb.kuhl@gmail.com>
2024-02-06 23:47:07 -0300

    Color generator working properly

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -1,17 +1,23 @@
-import { useState, useEffect } from "react";
 import dynamic from "next/dynamic";
-import useRandColor from "@/hooks/useRandColor";
+import useRgb from "@/hooks/useRgb";
 import Button from "@/components/Button"
 
 const NoSSR = dynamic(() => import("../components/ColorCode"), {ssr: false})
 
 export default function Generator() {
-  const { color, setColor, newColor, changeColor, isCode, setIsCode } = useRandColor();
+  const { rgb, generateRgb } = useRgb();
 
   return (
-    <div className="flex flex-col">
-      <Button />
-      <NoSSR />
+    <div
+      className="flex justify-center items-center h-screen"
+      style={{
+        backgroundColor: rgb,
+      }}
+    >
+      <div className="flex flex-col w-72">
+        <Button func={generateRgb} />
+        <NoSSR text={rgb} />
+      </div>
     </div>
   );
 }
