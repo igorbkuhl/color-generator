8d132ff492a33de127b13d9fcb597a084eece1ff
igorbkuhl <igorb.kuhl@gmail.com>
2024-02-06 23:49:13 -0300

    Trigger function passed as props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,12 @@
-import { useEffect } from "react";
-import useRandColor from "@/hooks/useRandColor";
+interface ButtonProps {
+  func: () => void;
+}
 
-export default function Button() {
-  const { changeColor, setColor, newColor, generateColor } = useRandColor();
+export default function Button(props: ButtonProps) {
 
   return (
     <button
-      onClick={changeColor}
+      onClick={props.func}
       className="
       text-white p-4 border-2 border-white rounded-md
       bg-slate-800/50 backdrop-blur-sm"
