3dcfe6870c508d6c360c55903831bba55460d994
igorbkuhl <igorb.kuhl@gmail.com>
2024-02-17 12:52:02 -0300

    Color codes recieved as props

--- src/components/ColorCode.tsx
+++ src/components/ColorCode.tsx
@@ -1,15 +1,15 @@
-import useHex from "@/hooks/useHex";
-import useRgb from "@/hooks/useRgb";
+interface ColorCodeProps {
+  hex: string;
+  rgb: string;
+}
 
-export default function ColorCode() {
-  const { hex } = useHex(); 
-  const { rgb } = useRgb();
- 
+export default function ColorCode(props: ColorCodeProps) {
   return (
     <div className="bg-slate-800/50 backdrop-blur-sm
       mt-2 p-5 rounded-md text-center">
-        <p>{hex}</p>
-        <p>{rgb}</p>
+        <p>{props.hex}</p>
+        <p>{props.rgb}</p>
     </div>
   )
 }
+
