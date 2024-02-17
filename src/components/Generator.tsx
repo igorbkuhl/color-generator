03ef654620dc4c59a87b06af45a207d207ebb024
igorbkuhl <igorb.kuhl@gmail.com>
2024-02-17 12:56:04 -0300

    Triggering new color generations

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -1,22 +1,48 @@
-import dynamic from "next/dynamic";
+import { useEffect } from "react";
 import useRgb from "@/hooks/useRgb";
-import Button from "@/components/Button"
-
-const NoSSR = dynamic(() => import("../components/ColorCode"), {ssr: false})
+import useHex from "@/hooks/useHex";
+import Button from "@/components/Button";
+import ColorCode from "./ColorCode";
 
 export default function Generator() {
-  const { rgb, generateRgb } = useRgb();
+  const { hex, generateHex } = useHex();
+  const { rgb, hexToRgb } = useRgb();
+  const colors = {
+    hex: `#${hex[0]}${hex[1]}${hex[2]}`,
+    rgb: `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`,
+  }
+
+  useEffect(() => {
+    const newHex = generateHex();
+    console.log(newHex);
+    hexToRgb(newHex);
+    console.log(rgb);
+  }, [])
 
   return (
     <div
       className="flex justify-center items-center h-screen"
       style={{
-        backgroundColor: rgb,
+        backgroundColor: colors.hex,
       }}
     >
       <div className="flex flex-col w-72">
-        <Button func={generateRgb} />
-        <NoSSR text={rgb} />
+        <Button func={() => {
+          const newHex = generateHex();
+          console.log(newHex);
+          hexToRgb(newHex);
+          console.log(rgb);
+          console.groupCollapsed(
+            "%c" + hex,
+            `background-color: ${colors.hex}`
+          )
+          console.table(colors)
+          console.groupEnd();
+        }} />
+        <ColorCode
+          hex={colors.hex}
+          rgb={colors.rgb}
+        />
       </div>
     </div>
   );
