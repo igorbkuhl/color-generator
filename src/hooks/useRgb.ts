b80c72800ea8a9580e238aff3fb5f25590075306
igorbkuhl <igorb.kuhl@gmail.com>
2024-02-18 06:05:51 -0300

    useRgb now generates a RGB color

--- src/hooks/useRgb.ts
+++ src/hooks/useRgb.ts
@@ -4,38 +4,18 @@ export default function useRgb() {
   const [rgb, setRgb] = useState([0, 0, 0]);
   const rgbArray: number[] = [];
 
-  const hexToRgb = (hex: string[]) => {
-    for (let i = 0; i < 3; i++) {
-      const rgbNumber = parseInt(hex[i], 16);
-      rgbArray.push(rgbNumber);
-    }
+  const generateRgb = () => {
+    rgbArray.push(
+      Math.trunc(Math.random() * 256),
+      Math.trunc(Math.random() * 256),
+      Math.trunc(Math.random() * 256),
+    );
     setRgb(rgbArray);
+    return rgbArray;
   }
 
   return {
     rgb,
-    hexToRgb
-  }
+    generateRgb
+  };
 }
-
-
-// export default function useRgb() {
-//   const [rgb, setRgb] = useState([0, 0, 0]);
-//   let rgbArray: number[] = [];
-
-//   const generateRgb = () => {
-//     rgbArray = [
-//       Math.trunc(Math.random() * 256),
-//       Math.trunc(Math.random() * 256),
-//       Math.trunc(Math.random() * 256),
-//     ]
-//     setRgb(rgbArray);
-//   }
-
-//   return {
-//     rgb,
-//     setRgb,
-//     rgbArray,
-//     generateRgb
-//   };
-// }
