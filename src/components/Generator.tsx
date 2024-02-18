bad8a030569b49c71f05dc1bf89ca02e95932c54
igorbkuhl <igorb.kuhl@gmail.com>
2024-02-18 06:56:43 -0300

    Added HSL converter

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -1,21 +1,25 @@
 import { useEffect } from "react";
-import useRgb from "@/hooks/useRgb";
 import useHex from "@/hooks/useHex";
+import useRgb from "@/hooks/useRgb";
+import useHsl from "@/hooks/useHsl";
 import Button from "@/components/Button";
 import ColorCode from "./ColorCode";
 
 export default function Generator() {
   const { hex, rgbToHex } = useHex();
   const { rgb, generateRgb } = useRgb();
+  const { hsl, rgbToHsl } = useHsl();
   const colors = {
     hex: `#${hex[0]}${hex[1]}${hex[2]}`,
     rgb: `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`,
-  }
+    hsl: `hsl(${hsl[0]}º, ${hsl[1]}%, ${hsl[2]}%)`,
+  };
 
   useEffect(() => {
     const newRgb = generateRgb();
     rgbToHex(newRgb);
-  }, [])
+    rgbToHsl(newRgb);
+  }, []);
 
   return (
     <div
@@ -25,20 +29,20 @@ export default function Generator() {
       }}
     >
       <div className="flex flex-col w-72">
-        <Button func={() => {
-          const newRgb = generateRgb();
-          rgbToHex(newRgb);
-          console.groupCollapsed(
-            "%c" + colors.hex,
-            `background-color: ${colors.hex}`
-          )
-          console.table(colors)
-          console.groupEnd();
-        }} />
-        <ColorCode
-          hex={colors.hex}
-          rgb={colors.rgb}
+        <Button
+          func={() => {
+            const newRgb = generateRgb();
+            rgbToHex(newRgb);
+            rgbToHsl(newRgb);
+            console.groupCollapsed(
+              "%c" + colors.hex,
+              `background-color: ${colors.hex}`
+            );
+            console.table(colors);
+            console.groupEnd();
+          }}
         />
+        <ColorCode hex={colors.hex} rgb={colors.rgb} hsl={colors.hsl} />
       </div>
     </div>
   );
