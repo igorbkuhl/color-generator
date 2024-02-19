5540a75b789f6a9af4cb55911755acf2508cf668
igorbkuhl <igorb.kuhl@gmail.com>
2024-02-19 13:11:30 -0300

    Component rendering simplified

--- src/components/ColorCode.tsx
+++ src/components/ColorCode.tsx
@@ -1,22 +1,13 @@
 interface ColorCodeProps {
-  hex: string;
-  rgb: string;
-  hsl: string;
-  hsv: string;
-  cmyk: string;
-  oklab: string;
+  children: any;
 }
 
 export default function ColorCode(props: ColorCodeProps) {
   return (
     <div className="bg-slate-800/50 backdrop-blur-sm
-      mt-2 p-5 rounded-md text-center">
-        <p>{props.hex}</p>
-        <p>{props.rgb}</p>
-        <p>{props.hsl}</p>
-        <p>{props.hsv}</p>
-        <p>{props.cmyk}</p>
-        <p>{props.oklab}</p>
+      mt-2 p-5 rounded-md text-center"
+    >
+      {props.children}
     </div>
   )
 }
