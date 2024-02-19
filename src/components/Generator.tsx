e7e6df9efeacdd0edc8589e734e6d143a5ba56e2
igorbkuhl <igorb.kuhl@gmail.com>
2024-02-19 10:09:00 -0300

    Added CMYK converter

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -5,18 +5,21 @@ import useHex from "@/hooks/useHex";
 import useRgb from "@/hooks/useRgb";
 import useHsl from "@/hooks/useHsl";
 import useHsv from "@/hooks/useHsv";
+import useCmyk from "@/hooks/useCmyk";
 
 export default function Generator() {
   const { hex, rgbToHex } = useHex();
   const { rgb, generateRgb } = useRgb();
   const { hsl, rgbToHsl } = useHsl();
   const { hsv, rgbToHsv } = useHsv();
+  const { cmyk, rgbToCmyk } = useCmyk();
 
   const colors = {
     hex: `#${hex[0]}${hex[1]}${hex[2]}`,
     rgb: `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`,
     hsl: `hsl(${hsl[0]}º, ${hsl[1]}%, ${hsl[2]}%)`,
     hsv: `hsv(${hsv[0]}º, ${hsv[1]}%, ${hsv[2]}%)`,
+    cmyk: `cmyk(${cmyk[0]}%, ${cmyk[1]}%, ${cmyk[2]}%, ${cmyk[3]}%)`,
   };
 
   useEffect(() => {
@@ -24,6 +27,7 @@ export default function Generator() {
     rgbToHex(newRgb);
     rgbToHsl(newRgb);
     rgbToHsv(newRgb);
+    rgbToCmyk(newRgb);
   }, []);
 
   return (
@@ -40,6 +44,7 @@ export default function Generator() {
             rgbToHex(newRgb);
             rgbToHsl(newRgb);
             rgbToHsv(newRgb);
+            rgbToCmyk(newRgb);
             console.groupCollapsed(
               "%c" + colors.hex,
               `background-color: ${colors.hex}`
@@ -53,6 +58,7 @@ export default function Generator() {
           rgb={colors.rgb}
           hsl={colors.hsl}
           hsv={colors.hsv}
+          cmyk={colors.cmyk}
         />
       </div>
     </div>
