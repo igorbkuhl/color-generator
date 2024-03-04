ccdbfb46a192bfe5ce5dfdff09f45474744221cd
montizz <igorb.kuhl@gmail.com>
2024-03-04 12:42:30 -0300

    Background color changes with generated RGB and received other styles

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
-import RandomGenerator from './components/RandomGenerator.vue'
+import RandomGenerator from "./components/RandomGenerator.vue";
+import { useRgbStore } from "./stores/rgbStore";
+
+const rgb = useRgbStore();
+const newRgb = rgb.generateRgb();
 </script>
 
 <template>
@@ -7,3 +11,15 @@ import RandomGenerator from './components/RandomGenerator.vue'
     <RandomGenerator />
   </main>
 </template>
+
+<style scoped>
+main {
+  background-color: rgb(v-bind(newRgb));
+  height: 100vh;
+  width: 100vw;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
