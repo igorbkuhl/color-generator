584a2326a8bdffeb7bc53957b670d5eff09d0468
montizz <igorb.kuhl@gmail.com>
2024-03-04 12:43:57 -0300

    Added RGB color code

--- src/components/ColorCode.vue
+++ src/components/ColorCode.vue
@@ -1,9 +1,21 @@
-<script setup lang="ts">
-defineProps<{ hex: string }>()
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ColorCode",
+  props: {
+    rgb: String,
+  },
+  setup(props) {
+    const rgb = props.rgb;
+
+    return rgb;
+  },
+});
 </script>
 
 <template>
-  <p>{{ hex }}</p>
+  <p>{{ rgb }}</p>
 </template>
 
 <style scoped>
