d46d1482dbf5d4228f4d55e1b5f4fa8caccbe640
montizz <igorb.kuhl@gmail.com>
2024-03-05 12:47:22 -0300

    Added HEX color code

--- src/components/ColorCode.vue
+++ src/components/ColorCode.vue
@@ -4,20 +4,17 @@ import { defineComponent } from "vue";
 export default defineComponent({
   name: "ColorCode",
   props: {
+    hex: String,
     rgb: String,
-  },
-  setup(props) {
-    const rgb = props.rgb;
-
-    return rgb;
   },
 });
 </script>
 
 <template>
-  <p class="bg-slate-800/50 backdrop-blur-sm
+  <div class="bg-slate-800/50 backdrop-blur-sm
   mt-2 p-5 rounded-md text-center"
   >
-    {{ rgb }}
-  </p>
+    <p>{{ $props.hex }}</p>
+    <p>{{ $props.rgb }}</p>
+  </div>
 </template>
