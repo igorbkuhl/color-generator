1207148f596bceb5f556e3b160fc2a8bcd0820a8
montizz <igorb.kuhl@gmail.com>
2024-03-05 12:48:06 -0300

    New color function triggered with props

--- src/components/GenerateButton.vue
+++ src/components/GenerateButton.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import { useRgbStore } from "../stores/rgbStore";
-
-let rgb = useRgbStore();
+defineProps<{
+  func: () => void;
+}>()
 </script>
 
 <template>
   <button
-    @click="rgb.generateRgb()"
+    @click="func"
     class="text-white p-4 border-2 border-white rounded-md
     bg-slate-800/50 backdrop-blur-sm"
   >
