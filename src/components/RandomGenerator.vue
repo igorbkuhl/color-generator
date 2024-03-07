b47dcbd951c8ef111f2e7ac67239f89d059aebd7
monitzz <igorb.kuhl@gmail.com>
2024-03-07 06:23:52 -0300

    Added RGB to HSV converter

--- src/components/RandomGenerator.vue
+++ src/components/RandomGenerator.vue
@@ -4,15 +4,18 @@ import ColorCode from "./ColorCode.vue";
 import { useRgbStore } from "../stores/rgbStore";
 import { useHexStore } from "../stores/hexStore";
 import { useHslStore } from "../stores/hslStore";
+import { useHsvStore } from "../stores/hsvStore";
 
 const rgb = useRgbStore();
 const hex = useHexStore();
 const hsl = useHslStore();
+const hsv = useHsvStore();
 
 const newColor = () => {
   const newRgb = rgb.generateRgb();
   hex.toHex(newRgb);
   hsl.toHsl(newRgb);
+  hsv.toHsv(newRgb)
 };
 </script>
 
@@ -27,6 +30,7 @@ const newColor = () => {
         <p>{{ hex.fullHex }}</p>
         <p>{{ rgb.fullRgb }}</p>
         <p>{{ hsl.fullHsl }}</p>
+        <p>{{ hsv.fullHsv }}</p>
       </ColorCode>
     </div>
   </div>
