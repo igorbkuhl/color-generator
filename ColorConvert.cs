abab65928c0a7b8ccdd97289fefb3f9278f35b78
monitzz <igorb.kuhl@gmail.com>
2024-05-24 11:32:04 -0300

    Created class for color converters.

---
+++ ColorConvert.cs
@@ -0,0 +1,20 @@
+﻿namespace RandomColorGenerator;
+
+class ColorConvert
+{
+    public static string[] hexList = new string[3];
+
+    public static void FromRgbToHex(byte[] rgb)
+    {
+        for (int i = 0; i < hexList.Length; i++)
+        {
+            string hexNum = Convert.ToString(rgb[i], 16);
+
+            if (hexNum.Length == 1)
+            {
+                hexNum = "0" + hexNum;
+            }
+            hexList[i] = hexNum;
+        }
+    }
+}
