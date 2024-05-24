fc1fc12b38cae0439bce9d4e08140eabdd454406
monitzz <igorb.kuhl@gmail.com>
2024-05-24 15:04:58 -0300

    Added a RGB to HSL converter.

--- ColorConvert.cs
+++ ColorConvert.cs
@@ -3,6 +3,7 @@
 class ColorConvert
 {
     public static string[] hexList = new string[3];
+    public static float[] hslList = new float[3];
 
     public static void FromRgbToHex(byte[] rgb)
     {
@@ -16,5 +17,67 @@ class ColorConvert
             }
             hexList[i] = hexNum;
         }
+    }
+
+    public static void FromRgbToHsl(byte[] rgb)
+    {
+        float[] decimalRgb =
+        [
+            (float)rgb[0] / 255,
+            (float)rgb[1] / 255,
+            (float)rgb[2] / 255
+        ];
+        float max = float.MinValue,
+            min = float.MaxValue;
+        float d;
+        float[] hsl = new float[3];
+
+        foreach (float value in decimalRgb)
+        {
+            if (value > max)
+            {
+                max = value;
+            }
+
+            if (value < min)
+            {
+                min = value;
+            }
+        }
+
+        d = max - min;
+        hsl[2] = (min + max) / 2;
+
+        if (d == 0)
+        {
+            hsl[1] = 0;
+        }
+        else
+        {
+            hsl[1] = d / (1 - Math.Abs(2 * hsl[2] - 1));
+        }
+
+        if (d == 0)
+        {
+            hsl[0] = 0;
+        }
+        else if (max == decimalRgb[0])
+        {
+            hsl[0] = ((((decimalRgb[1] - decimalRgb[2]) / d) % 6) + 6) % 6;
+        }
+        else if (max == decimalRgb[1])
+        {
+            hsl[0] = (decimalRgb[2] - decimalRgb[0]) / d + 2;
+        }
+        else if (max == decimalRgb[2])
+        {
+            hsl[0] = (decimalRgb[0] - decimalRgb[1]) / d + 4;
+        }
+
+        hsl[0] = (float)Math.Round(hsl[0] * 60);
+        hsl[1] = (float)Math.Round(hsl[1] * 100);
+        hsl[2] = (float)Math.Round(hsl[2] * 100);
+
+        hslList = hsl;
     }
 }
